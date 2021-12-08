5ff8671aff06137ef7c286edc7a9e13d9a4a805b
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2021-12-08 15:37:49 -0500

    day 5 part 2

--- src/05.jl
+++ src/05.jl
@@ -1,3 +1,5 @@
+using Base.Iterators
+
 struct Line{N <: Integer}
     x1::N
     y1::N
@@ -16,29 +18,59 @@ function find_dims(lines)
         w =  max(line.x1, line.x2, w)
         h =  max(line.y1, line.y2, h)
     end
-    w, h
+    return w, h
 end
 
 function solve1(lines)
     w, h = find_dims(lines)
     M = zeros(Int, w, h)
     for line in lines
-        @show line
         xl, xr = minmax(line.x1, line.x2)
         yd, yu = minmax(line.y1, line.y2)
-        if xl ≠ xr && yd ≠ yu
+        if xl != xr && yd != yu
             continue
         end
         for i in xl:xr, j in yd:yu
             M[i, j] += 1
         end
     end
-    count(>=(2), M)
+    return count(>=(2), M)
+end
+
+function solve2(lines)
+    w, h = find_dims(lines)
+    M = zeros(Int, w, h)
+    for line in lines
+        xrange =
+            if line.x1 == line.x2
+                cycle(line.x1)
+            elseif line.x1 < line.x2
+                range(line.x1, line.x2; step=1)
+            else
+                range(line.x1, line.x2; step=-1)
+            end
+
+        yrange =
+            if line.y1 == line.y2
+                cycle(line.y1)
+            elseif line.y1 < line.y2
+                range(line.y1, line.y2; step=1)
+            else
+                range(line.y1, line.y2; step=-1)
+            end
+
+        for (i, j) in zip(xrange, yrange)
+            M[i, j] += 1
+        end
+    end
+    return count(>=(2), M)
 end
 
 function main()
     path = abspath(joinpath(@__DIR__, "..", "data/", "05.in"))
-    solve1(read_input(path))
+    input = read_input(path)
+    solve1(input) |> println
+    solve2(input) |> println
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
